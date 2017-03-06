1208cf5aa6490da72b9a5124d005bd85641e3e0a
FiniteReality <FiniteReality@users.noreply.github.com>
2017-03-06 18:45:51 +0000

    Finally fix the build script

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-if [ -d "./build" ]; then
+if ! [ -d "./build" ]; then
     mkdir build
 fi
 
 cd build
 
-# if we are not a travis CI build
-if [ "$TRAVIS" -eq "true" -a "$TRAVIS_PULL_REQUEST_SHA" -eq "" ]; then
+# if we are not a travis CI PR build
+if [ "$TRAVIS" = "true" -a "$TRAVIS_PULL_REQUEST_SHA" = "" ]; then
     cmake ..
 else
     cmake .. -DDISCCORD_IGNORE_UNSAFE_TESTS=1
