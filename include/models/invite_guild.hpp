51cd5dca48515e44a73f6caea37656c3986e2ef0
Ashton Tito <ashton_tito@sfu.ca>
2017-03-10 13:36:32 -0800

    build invite guild model

---
+++ include/models/invite_guild.hpp
@@ -0,0 +1,33 @@
+#ifndef _invite_guild_hpp_
+#define _invite_guild_hpp_
+
+#include <models/entity.hpp>
+
+namespace disccord
+{
+    namespace models
+    {
+        class invite_guild : public model
+        {
+            public:
+                invite_guild();
+                virtual ~invite_guild();
+
+                virtual void decode(web::json::value json) override;
+
+                std::string get_id();
+                std::string get_name();
+                util::optional<std::string> get_splash();
+                util::optional<std::string> get_icon();
+
+            protected:
+                virtual void encode_to(std::unordered_map<std::string, web::json::value> &info) override;
+
+            private:
+                std::string id, name;
+                util::optional<std::string> splash, icon;
+        };
+    }
+}
+
+#endif /* _invite_guild_hpp_ */
---
+++ lib/models/invite_guild.cpp
@@ -0,0 +1,58 @@
+#include <models/invite_guild.hpp>
+
+namespace disccord
+{
+    namespace models
+    {
+        invite_guild::invite_guild()
+            : id(""), name(""),splash(),icon()
+        { }
+
+        invite_guild::~invite_guild()
+        { }
+
+        void invite_guild::decode(web::json::value json)
+        {
+            id = json.at("id").as_string();
+            name = json.at("name").as_string();
+            #define get_field(var, conv) \
+                if (json.has_field(#var)) { \
+                    auto field = json.at(#var); \
+                    if (!field.is_null()) { \
+                        var = decltype(var)(field.conv()); \
+                    } else { \
+                        var = decltype(var)::no_value(); \
+                    } \
+                } else { \
+                    var = decltype(var)(); \
+                }
+
+            get_field(splash, as_string);
+            get_field(icon, as_string);
+
+            #undef get_field
+        }
+
+        void invite_guild::encode_to(std::unordered_map<std::string, web::json::value> &info)
+        {
+            info["name"] = web::json::value(name);
+            info["id"] = web::json::value(id);
+            if (splash.is_specified())
+                info["splash"] = web::json::value(splash.get_value());
+            if (icon.is_specified())
+                info["icon"] = web::json::value(icon.get_value());
+        }
+        
+        #define define_get_method(field_name) \
+            decltype(invite_guild::field_name) invite_guild::get_##field_name() { \
+                return field_name; \
+            }
+            
+        define_get_method(name);
+        define_get_method(id);
+        define_get_method(splash);
+        define_get_method(icon);
+        
+        #undef define_get_method
+    }
+}
