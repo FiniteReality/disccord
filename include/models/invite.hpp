ad931f91a5e934077e1260d0804eba60b517bfe5
Ashton Tito <ashton_tito@sfu.ca>
2017-03-10 13:36:52 -0800

    build invite model

---
+++ include/models/invite.hpp
@@ -0,0 +1,35 @@
+#ifndef _invite_hpp_
+#define _invite_hpp_
+
+#include <models/entity.hpp>
+#include <models/invite_channel.hpp>
+#include <models/invite_guild.hpp>
+
+namespace disccord
+{
+    namespace models
+    {
+        class invite : public model
+        {
+            public:
+                invite();
+                virtual ~invite();
+
+                virtual void decode(web::json::value json) override;
+
+                std::string get_code();
+                util::optional<invite_channel> get_channel();
+                util::optional<invite_guild> get_guild();
+
+            protected:
+                virtual void encode_to(std::unordered_map<std::string, web::json::value> &info) override;
+
+            private:
+                std::string code;
+                util::optional<invite_channel> channel;
+                util::optional<invite_guild> guild;
+        };
+    }
+}
+
+#endif /* _invite_hpp_ */
---
+++ lib/models/invite.cpp
@@ -0,0 +1,59 @@
+#include <models/invite.hpp>
+
+namespace disccord
+{
+    namespace models
+    {
+        invite::invite()
+            : code(""),channel(),guild()
+        { }
+
+        invite::~invite()
+        { }
+
+        void invite::decode(web::json::value json)
+        {
+            code = json.at("code").as_string();
+
+            #define get_composite_field(var, type) \
+                if (json.has_field(#var)) { \
+                    auto field = json.at(#var); \
+                    if (!field.is_null()) { \
+                        type val; \
+                        val.decode(field); \
+                        var = decltype(var)(val); \
+                    } else { \
+                        var = decltype(var)::no_value(); \
+                    } \
+                } else { \
+                    var = decltype(var)(); \
+                }
+
+            get_composite_field(guild, invite_guild);
+            get_composite_field(channel, invite_channel);
+            
+            #undef get_composite_field
+        }
+
+        void invite::encode_to(std::unordered_map<std::string, web::json::value> &info)
+        {
+            info["code"] = web::json::value(code);
+            
+            if (guild.is_specified())
+                info["guild"] = guild.get_value().encode();
+            if (channel.is_specified())
+                info["channel"] = channel.get_value().encode();
+        }
+
+        #define define_get_method(field_name) \
+            decltype(invite::field_name) invite::get_##field_name() { \
+                return field_name; \
+            }
+
+        define_get_method(code);
+        define_get_method(channel);
+        define_get_method(guild);
+
+        #undef define_get_method
+    }
+}
