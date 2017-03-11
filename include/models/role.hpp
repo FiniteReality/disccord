38be94b06100b2997f2cdc01984c89f517163870
Ashton Tito <ashton_tito@sfu.ca>
2017-03-11 01:51:53 -0800

    add role model

---
+++ include/models/role.hpp
@@ -0,0 +1,39 @@
+#ifndef _role_hpp_
+#define _role_hpp_
+
+#include <models/entity.hpp>
+
+namespace disccord
+{
+    namespace models
+    {
+        class role : public model
+        {
+            public:
+                role();
+                virtual ~role();
+
+                virtual void decode(web::json::value json) override;
+
+                std::string get_id();
+                std::string get_name();
+                uint32_t get_color();
+                uint32_t get_permissions();
+                uint8_t get_position();
+                bool get_managed();
+                bool get_mentionable();
+                bool get_hoist();
+
+            protected:
+                virtual void encode_to(std::unordered_map<std::string, web::json::value> &info) override;
+
+            private:
+                std::string id, name;
+                uint32_t color, permissions;
+                uint8_t position;
+                bool managed, mentionable, hoist;
+        };
+    }
+}
+
+#endif /* _role_hpp_ */
