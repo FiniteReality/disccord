e0247f1697a66be4932bc7e16701a019af29e912
Ashton Tito <ashton_tito@sfu.ca>
2017-03-16 16:59:17 -0700

    add application model

---
+++ include/models/application.hpp
@@ -0,0 +1,43 @@
+#ifndef _application_hpp_
+#define _application_hpp_
+
+#include <models/entity.hpp>
+#include <models/user.hpp>
+
+namespace disccord
+{
+    namespace models
+    {
+        class application : public model
+        {
+            public:
+                application();
+                virtual ~application();
+
+                virtual void decode(web::json::value json) override;
+
+                uint64_t get_id();
+                std::string get_name();
+                util::optional<std::string> get_icon();
+                util::optional<std::string> get_description();
+                util::optional<std::vector<std::string>> get_rpc_origins();
+                bool get_bot_public();
+                bool get_bot_requires_code_grant();
+                util::optional<user> get_owner();
+
+            protected:
+                virtual void encode_to(std::unordered_map<std::string, web::json::value> &info) override;
+
+            private:
+                uint64_t id;
+                std::string name;
+                util::optional<std::string> icon, description;
+                util::optional<std::vector<std::string>> rpc_origins;
+                bool bot_public, bot_requires_code_grant;
+                util::optional<user> owner;
+                
+        };
+    }
+}
+
+#endif /* _application_hpp_ */
