3f6937b8f56fe7824616869b7baeae3de9a191a6
Ashton Tito <ashton_tito@sfu.ca>
2017-03-16 02:17:48 -0700

    add game and presence models

---
+++ lib/models/game.cpp
@@ -0,0 +1,57 @@
+#include <boost/lexical_cast.hpp>
+
+#include <models/game.hpp>
+
+namespace disccord
+{
+    namespace models
+    {
+        game::game()
+        : name(""), url(), type()
+        { }
+
+        game::~game()
+        { }
+
+        void game::decode(web::json::value json)
+        {
+            name = json.at("name").as_string();
+            
+            #define get_field(var, conv) \
+                if (json.has_field(#var)) { \
+                    auto field = json.at(#var); \
+                    if (!field.is_null()) { \
+                        var = decltype(var)(field.conv()); \
+                    } else { \
+                        var = decltype(var)::no_value(); \
+                    } \
+                } else { \
+                    var = decltype(var)(); \
+                }
+
+            get_field(url, as_string);
+            get_field(type, as_integer);
+            
+            #undef get_field
+        }
+
+        void game::encode_to(std::unordered_map<std::string, web::json::value> &info)
+        {
+            info["name"] = web::json::value::string(name);
+            if (url.is_specified())
+                info["url"] = web::json::value::string(url);
+            if (type.is_specified())
+                info["type"] = web::json::value::number(type);
+        }
+        
+        #define define_get_method(field_name) \
+            decltype(game::field_name) game::get_##field_name() { \
+                return field_name; \
+            }
+        define_get_method(name);
+        define_get_method(url);
+        define_get_method(type);
+        
+        #undef define_get_method
+    }
+}
---
+++ lib/models/presence.cpp
@@ -0,0 +1,84 @@
+#include <boost/lexical_cast.hpp>
+
+#include <models/presence.hpp>
+
+namespace disccord
+{
+    namespace models
+    {
+        presence::presence()
+        : user(), roles(), game(),
+        guild_id(0), status("")
+        { }
+
+        presence::~presence()
+        { }
+
+        void presence::decode(web::json::value json)
+        {
+            status = json.at("status").as_string();
+            guild_id = boost::lexical_cast<uint64_t>(json.at("guild_id").as_string());
+            
+            #define get_composite_field(var, type) \
+                if (json.has_field(#var)) { \
+                    auto field = json.at(#var); \
+                    if (!field.is_null()) { \
+                        type val; \
+                        val.decode(field); \
+                        var = decltype(var)(val); \
+                    } else { \
+                        var = decltype(var)::no_value(); \
+                    } \
+                } else { \
+                    var = decltype(var)(); \
+                }
+
+            get_composite_field(user, models::user);
+            get_composite_field(game, models::game);
+            
+            if (json.has_field("roles"))
+            {
+                auto _roles_array = json.at("roles").as_array();
+                std::vector<uint64_t> roles_array(_roles_array.size());
+                std::transform(_roles_array.begin(), _roles_array.end(), roles_array.begin(),
+                    [](web::json::value _role){
+                        return _role.as_integer();
+                    });
+                roles = roles_array;
+            }
+            
+            #undef get_composite_field
+        }
+
+        void presence::encode_to(std::unordered_map<std::string, web::json::value> &info)
+        {
+            info["status"] = web::json::value::string(status);
+            info["guild_id"] = web::json::value::number(guild_id);
+            if (user.is_specified())
+                info["user"] = user.get_value().encode();
+            if (game.is_specified())
+                info["game"] = game.get_value().encode();
+            {
+                auto _roles = roles;
+                std::vector<web::json::value> roles_array(_roles.size());
+                std::transform(_roles.begin(), _roles.end(), roles_array.begin(),
+                    [](uint64_t role){
+                        return web::json::value(role);
+                    });
+                info["roles"] = web::json::value::array(roles_array);
+            }
+        }
+        
+        #define define_get_method(field_name) \
+            decltype(presence::field_name) presence::get_##field_name() { \
+                return field_name; \
+            }
+        define_get_method(status);
+        define_get_method(guild_id);
+        define_get_method(user);
+        define_get_method(game);
+        define_get_method(roles);
+        
+        #undef define_get_method
+    }
+}
