2661c032d69a5d60127c0b2842671c98c00d3cf6
Ashton Tito <ashton_tito@sfu.ca>
2017-03-17 03:48:56 -0700

    add more tests for models

--- tests/models.cpp
+++ tests/models.cpp
@@ -7,12 +7,20 @@
 #include <models/message.hpp>
 #include <models/channel.hpp>
 #include <models/connection.hpp>
+#include <models/guild.hpp>
+#include <models/guild_embed.hpp>
+#include <models/read_state.hpp>
+#include <models/relationship.hpp>
+#include <models/ban.hpp>
+#include <models/application.hpp>
+#include <util/optional.hpp>
 
 #include <iostream>
 #include <fstream>
 
 #include <cstdlib>
 
+using namespace disccord;
 using namespace disccord::models;
 
 TEST_CASE( "User model correctly instantiated" ){
@@ -28,7 +36,7 @@ TEST_CASE( "User model correctly instantiated" ){
     "bot": false
 })";
 
-    test_user.decode(web::json::value::parse(json));
+    REQUIRE_NOTHROW(test_user.decode(web::json::value::parse(json)));
     
     REQUIRE(test_user.get_id() == 1234567890);
     REQUIRE(test_user.get_username() == "FiniteReality");
@@ -85,7 +93,7 @@ TEST_CASE( "Embed model correctly instantiated" ){
     ]
 })";
 
-    test_embed.decode(web::json::value::parse(json));
+    REQUIRE_NOTHROW(test_embed.decode(web::json::value::parse(json)));
 
     // TODO: put some requires here!!
 }
@@ -111,7 +119,7 @@ TEST_CASE( "Invite model correctly instantiated" ){
     }
 })";
 
-    test_invite.decode(web::json::value::parse(json));
+    REQUIRE_NOTHROW(test_invite.decode(web::json::value::parse(json)));
     
     // When splash/icon are null
     REQUIRE(test_invite.get_code() == "asdfh487yed");
@@ -136,7 +144,7 @@ TEST_CASE( "Invite model correctly instantiated" ){
     }
 })";
 
-    test_invite.decode(web::json::value::parse(json2));
+    REQUIRE_NOTHROW(test_invite.decode(web::json::value::parse(json2)));
     
     // When splash/icon have values
     REQUIRE(test_invite.get_guild().get_value().get_splash().get_value() == "9de5c5675676fdabbc54c52ca03860a1");
@@ -166,7 +174,7 @@ TEST_CASE( "Invite Metadata model correctly instantiated" ){
     "revoked" : false
 })";
     
-    test_invite_md.decode(web::json::value::parse(json));
+    REQUIRE_NOTHROW(test_invite_md.decode(web::json::value::parse(json)));
     
     REQUIRE(test_invite_md.get_inviter().get_value().get_id() == 1234567890);
     REQUIRE(test_invite_md.get_inviter().get_value().get_username() == "FiniteReality");
@@ -241,7 +249,7 @@ TEST_CASE( "Message model correctly instantiated" ){
     "pinned" : false
 })";
 
-    test_message.decode(web::json::value::parse(json));
+    REQUIRE_NOTHROW(test_message.decode(web::json::value::parse(json)));
     
     REQUIRE(test_message.get_author().get_value().get_id() == 1234567890);
     REQUIRE(test_message.get_author().get_value().get_username() == "FiniteReality");
@@ -306,7 +314,7 @@ TEST_CASE( "Channel model correctly instantiated" ){
     "last_message_id" : "153642275539255296"
 })";
 
-    test_channel.decode(web::json::value::parse(dm_json));
+    REQUIRE_NOTHROW(test_channel.decode(web::json::value::parse(dm_json)));
     
     REQUIRE(test_channel.get_id() == 134552934997426176);
     REQUIRE(test_channel.get_is_private() == true);
@@ -345,7 +353,7 @@ TEST_CASE( "Channel model correctly instantiated" ){
     "last_message_id": "155117677105512449"
 })";
     
-    test_channel2.decode(web::json::value::parse(textguild_json));
+    REQUIRE_NOTHROW(test_channel2.decode(web::json::value::parse(textguild_json)));
     
     REQUIRE(test_channel2.get_id() == 41771983423143937);
     REQUIRE(test_channel2.get_is_private() == false);
@@ -381,7 +389,7 @@ TEST_CASE( "Channel model correctly instantiated" ){
     "user_limit": 0
 })";
 
-    test_channel3.decode(web::json::value::parse(voiceguild_json));
+    REQUIRE_NOTHROW(test_channel3.decode(web::json::value::parse(voiceguild_json)));
     
     REQUIRE(test_channel3.get_id() == 155101607195836416);
     REQUIRE(test_channel3.get_is_private() == false);
@@ -452,7 +460,7 @@ TEST_CASE( "Connection model correctly instantiated" ){
     "revoked" : true
 })";
 
-    test_connection.decode(web::json::value::parse(json));
+    REQUIRE_NOTHROW(test_connection.decode(web::json::value::parse(json)));
     
     //integrations array
     auto ig1 = test_connection.get_integrations().get_value()[0];
@@ -497,4 +505,328 @@ TEST_CASE( "Connection model correctly instantiated" ){
     REQUIRE(test_connection.get_name() == "FiniteReality");
     REQUIRE(test_connection.get_type() == "twitch");
     REQUIRE(test_connection.get_revoked() == true);
+}
+
+TEST_CASE( "Guild model correctly instantiated" ){
+    guild test_guild;
+    if (test_guild.get_owner_id() != 0 && test_guild.get_afk_timeout() != 0 && 
+        !test_guild.get_region().empty() && !test_guild.get_name().empty() && 
+        test_guild.get_embed_enabled() && test_guild.get_verification_level() != 0
+        && test_guild.get_mfa_level() != 0 && test_guild.get_default_message_notifications() != 0)
+    {
+        FAIL("Default constructor for guild model not correctly instantiated");
+    }
+    
+    //testing a GUILD_CREATE scenario
+    std::string json = R"({
+        "id": "41771983423143937",
+        "name": "Discord Developers",
+        "icon": "SEkgTU9NIElUUyBBTkRSRUkhISEhISEh",
+        "splash": null,
+        "owner_id": "80351110224678912",
+        "region": "us-east",
+        "afk_channel_id": "42072017402331136",
+        "afk_timeout": 300,
+        "embed_enabled": true,
+        "embed_channel_id": "41771983444115456",
+        "verification_level": 1,
+        "default_message_notifications": 3,
+        "mfa_level": 1,
+        "roles": [
+            {
+                "id": "41771983423143936",
+                "name": "WE DEM BOYZZ!!!!!!",
+                "color": 3447003,
+                "hoist": true,
+                "position": 1,
+                "permissions": 66321471,
+                "managed": false,
+                "mentionable": false
+            },
+            {
+                "id": "23143936417719834",
+                "name": "WE DEM GURLZZ!!!!!!",
+                "color": 7003344,
+                "hoist": true,
+                "position": 5,
+                "permissions": 0,
+                "managed": true,
+                "mentionable": true
+            }
+        ],
+        "emojis": [
+            {
+                "id": "23143936417719834",
+                "name": "testemoj"
+            },
+            {
+                "id": "17719834231439364",
+                "name": "2testemoji2"
+            }
+        ],
+        "members": [
+            {
+                "user": {
+                    "id": "1234567890",
+                    "username": "FiniteReality",
+                    "discriminator": "5734",
+                    "bot": false
+                },
+                "nick": "NOT API SUPPORT",
+                "roles": [
+                    {
+                        "id": "23143936417719834",
+                        "name": "WE DEM GURLZZ!!!!!!",
+                        "color": 7003344,
+                        "hoist": true,
+                        "position": 5,
+                        "permissions": 0,
+                        "managed": true,
+                        "mentionable": true
+                    }
+                ],
+                "joined_at": "2015-04-26T06:26:56.936000+00:00",
+                "deaf": false,
+                "mute": false
+            },
+            {
+                "user": {
+                    "id": "1234567890",
+                    "username": "FiniteReality",
+                    "discriminator": "5734",
+                    "bot": false
+                },
+                "nick": "DISCCORD IS BEST LIB",
+                "roles": [
+                    {
+                        "id": "23143936417719834",
+                        "name": "WE DEM GURLZZ!!!!!!",
+                        "color": 7003344,
+                        "hoist": true,
+                        "position": 5,
+                        "permissions": 0,
+                        "managed": true,
+                        "mentionable": true
+                    }
+                ],
+                "joined_at": "2017-02-26T06:26:69.936000+00:00",
+                "deaf": true,
+                "mute": false
+            }
+        ],
+        "presences": [
+            {
+                "user": {
+                    "id": "1234567890",
+                    "username": "FiniteReality",
+                    "discriminator": "5734",
+                    "bot": false
+                },
+                "roles": ["34231771981439364","17719834393642314"],
+                "game": {
+                    "name": "Rocket League",
+                    "type": 1,
+                    "url": "https://www.twitch.tv/123"
+                },
+                "guild_id": "34177198143936423",
+                "status": "offline"
+            },
+            {
+                "user": {
+                    "id": "1234567890",
+                    "username": "FiniteReality",
+                    "discriminator": "5734",
+                    "bot": false
+                },
+                "roles": ["34231771981439364","17719834393642314"],
+                "game": {
+                    "name": "DisccordDev",
+                    "type": 0,
+                    "url": null
+                },
+                "guild_id": "19341778143936423",
+                "status": "online"
+            }
+        ],
+        "features": ["INVITE_SPLASH"],
+        "channels": [
+            {
+                "id": "41771983423143937",
+                "guild_id": "41771983423143937",
+                "name": "general",
+                "type": "text",
+                "position": 6,
+                "is_private": false,
+                "permission_overwrites": [],
+                "topic": "24/7 chat about how to gank Mike #2",
+                "last_message_id": "155117677105512449"
+            },
+            {
+                "id": "41771983423143937",
+                "guild_id": "41771983423143937",
+                "name": "general",
+                "type": "text",
+                "position": 6,
+                "is_private": false,
+                "permission_overwrites": [],
+                "topic": "24/7 chat about how to gank Mike #2",
+                "last_message_id": "155117677105512449"
+            }
+        ],
+        "unavailable": false
+})";
+
+    REQUIRE_NOTHROW(test_guild.decode(web::json::value::parse(json)));
+    auto null_string = util::optional<std::string>::no_value();
+    REQUIRE(test_guild.get_id() == 41771983423143937);
+    REQUIRE(test_guild.get_name() == "Discord Developers");
+    REQUIRE(test_guild.get_region() == "us-east");
+    REQUIRE(test_guild.get_icon().get_value() == "SEkgTU9NIElUUyBBTkRSRUkhISEhISEh");
+    CAPTURE(test_guild.get_splash().get_value());
+    REQUIRE(test_guild.get_owner_id() == 80351110224678912);
+    REQUIRE(test_guild.get_afk_channel_id().get_value()  == 42072017402331136);
+    REQUIRE(test_guild.get_embed_channel_id().get_value()  == 41771983444115456);
+    REQUIRE(test_guild.get_afk_timeout() == 300);
+    REQUIRE(test_guild.get_mfa_level() == 1);
+    REQUIRE(test_guild.get_verification_level() == 1);
+    REQUIRE(test_guild.get_default_message_notifications() == 3);
+    REQUIRE(test_guild.get_embed_enabled() == true);
+    
+    auto gfeats = test_guild.get_features()[0];
+    REQUIRE(gfeats == "INVITE_SPLASH");
+    
+    //only testing the composites/fields that still need code coverage
+    auto gpresc = test_guild.get_presences().get_value()[0]; //just going to test one value in vector for now
+    REQUIRE(gpresc.get_game().get_value().get_name() == "Rocket League");
+    REQUIRE(gpresc.get_game().get_value().get_url().get_value() == "https://www.twitch.tv/123");
+    REQUIRE(gpresc.get_game().get_value().get_type().get_value() == 1);
+    REQUIRE(gpresc.get_guild_id() == 34177198143936423);
+    REQUIRE(gpresc.get_status() == "offline");
+    auto gmembs = test_guild.get_members().get_value()[0]; //just going to test one value in vector for now
+    REQUIRE(gmembs.get_nick().get_value() == "NOT API SUPPORT");
+    REQUIRE(gmembs.get_joined_at() == "2015-04-26T06:26:56.936000+00:00");
+    REQUIRE(gmembs.get_deaf() == false);
+    REQUIRE(gmembs.get_mute() == false);
+}
+
+TEST_CASE( "Guild Embed model correctly instantiated" ){
+    guild_embed test_guild_embed;
+    if (test_guild_embed.get_channel_id() != 0 && test_guild_embed.get_enabled())
+    {
+        FAIL("Default constructor for guild embed model not correctly instantiated");
+    }
+    
+    std::string json = R"({
+    "channel_id": "38143936423417719",
+    "enabled": true
+})";
+
+    REQUIRE_NOTHROW(test_guild_embed.decode(web::json::value::parse(json)));
+    
+    REQUIRE(test_guild_embed.get_channel_id() == 38143936423417719);
+    REQUIRE(test_guild_embed.get_enabled() == true);
+}
+
+TEST_CASE( "Read State model correctly instantiated" ){
+    read_state test_read_state;
+    if (test_read_state.get_id() != 0 && test_read_state.get_mention_count() != 0)
+    {
+        FAIL("Default constructor for read state model not correctly instantiated");
+    }
+    
+    std::string json = R"({
+    "id": "64234381439317719",
+    "mention_count": 13,
+    "last_message_id": "64281439334317719"
+})";
+
+    REQUIRE_NOTHROW(test_read_state.decode(web::json::value::parse(json)));
+    
+    REQUIRE(test_read_state.get_id() == 64234381439317719);
+    REQUIRE(test_read_state.get_mention_count() == 13);
+    REQUIRE(test_read_state.get_last_message_id().get_value() == 64281439334317719);
+}
+
+TEST_CASE( "Relationship model correctly instantiated" ){
+    relationship test_relationship;
+    if (test_relationship.get_id() != 0)
+    {
+        FAIL("Default constructor for relationship model not correctly instantiated");
+    }
+    
+    std::string json = R"({
+    "id": "64234381431771993",
+    "user": {
+        "id": "1234567890",
+        "username": "FiniteReality",
+        "discriminator": "5734",
+        "bot": false
+    },
+    "type": 4
+})";
+
+    REQUIRE_NOTHROW(test_relationship.decode(web::json::value::parse(json)));
+    
+    REQUIRE(test_relationship.get_id() == 64234381431771993);
+    REQUIRE(test_relationship.get_type().get_value() == 4);
+}
+
+TEST_CASE( "Ban model correctly instantiated" ){
+    ban test_ban;
+    if (!test_ban.get_reason().empty())
+    {
+        FAIL("Default constructor for ban model not correctly instantiated");
+    }
+    
+    std::string json = R"({
+    "reason": "low quality jokes",
+    "user": {
+        "id": "1234567890",
+        "username": "FiniteReality",
+        "discriminator": "5734",
+        "bot": false
+    }
+})";
+
+    REQUIRE_NOTHROW(test_ban.decode(web::json::value::parse(json)));
+    
+    REQUIRE(test_ban.get_reason() == "low quality jokes");
+}
+
+TEST_CASE( "Application model correctly instantiated" ){
+    application test_app;
+    if (test_app.get_id() != 0  && !test_app.get_name().empty() &&
+        test_app.get_bot_public() && test_app.get_bot_requires_code_grant())
+    {
+        FAIL("Default constructor for application model not correctly instantiated");
+    }
+    
+    std::string json = R"({
+    "description": "Test",
+    "icon": null,
+    "id": "172150183260323840",
+    "name": "Baba O-Riley",
+    "bot_public": true,
+    "bot_requires_code_grant": false,
+    "owner": {
+        "id": "1234567890",
+        "username": "FiniteReality",
+        "discriminator": "5734",
+        "bot": false
+    },
+    "rpc_origins": [
+    "WHAT","THIS","APPLICATION"
+    ]
+})";
+
+    REQUIRE_NOTHROW(test_app.decode(web::json::value::parse(json)));
+    
+    REQUIRE(test_app.get_id() == 172150183260323840);
+    REQUIRE(test_app.get_name() == "Baba O-Riley");
+    REQUIRE(test_app.get_bot_public() == true);
+    REQUIRE(test_app.get_bot_requires_code_grant() == false);
+    REQUIRE(test_app.get_description().get_value() == "Test");
+    REQUIRE(test_app.get_rpc_origins().get_value()[0] == "WHAT");
+    REQUIRE(test_app.get_rpc_origins().get_value()[1] == "THIS");
+    REQUIRE(test_app.get_rpc_origins().get_value()[2] == "APPLICATION");
 }
