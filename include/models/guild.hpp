672b485674923587d3fb2262b14c48c61ff9c9cc
Ashton Tito <ashton_tito@sfu.ca>
2017-03-16 17:21:24 -0700

    add more fields to guild model

--- include/models/guild.hpp
+++ include/models/guild.hpp
@@ -2,6 +2,12 @@
 #define _guild_hpp_
 
 #include <models/entity.hpp>
+#include <models/role.hpp>
+#include <models/emoji.hpp>
+#include <models/guild_member.hpp>
+#include <models/channel.hpp>
+#include <models/presence.hpp>
+//#include <models/voice_state.hpp>
 #include <vector>
 
 namespace disccord
@@ -23,15 +29,18 @@ namespace disccord
                 std::string get_region();
                 util::optional<uint64_t> get_afk_channel_id();
                 int32_t get_afk_timeout();
+                int32_t get_mfa_level();
+                int32_t get_verification_level();
+                int32_t get_default_message_notifications();
                 bool get_embed_enabled();
                 util::optional<uint64_t> get_embed_channel_id();
-                // TODO: verification level
                 // TODO: voice states
-                // TODO: roles
-                // TODO: emojis
                 std::vector<std::string> get_features();
-                // TODO: mfa level
-                // TODO: default mesasge notifs
+                util::optional<std::vector<role>> get_roles();
+                util::optional<std::vector<emoji>> get_emojis();
+                util::optional<std::vector<guild_member>> get_members();
+                util::optional<std::vector<channel>> get_channels();
+                util::optional<std::vector<presence>> get_presences();
 
             protected:
                 virtual void encode_to(std::unordered_map<std::string,web::json::value> &info) override;
@@ -41,9 +50,14 @@ namespace disccord
                 util::optional<std::string> icon, splash;
                 uint64_t owner_id;
                 util::optional<uint64_t> afk_channel_id, embed_channel_id;
-                int32_t afk_timeout;
+                int32_t afk_timeout, mfa_level, verification_level, default_message_notifications;
                 bool embed_enabled;
                 std::vector<std::string> features;
+                util::optional<std::vector<role>> roles;
+                util::optional<std::vector<emoji>> emojis;
+                util::optional<std::vector<guild_member>> members;
+                util::optional<std::vector<channel>> channels;
+                util::optional<std::vector<presence>> presences;
         };
     }
 }
