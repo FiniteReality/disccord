29776575747026a342e9949eac6745cd6cc87dec
FiniteReality <FiniteReality@users.noreply.github.com>
2017-03-29 21:15:57 +0100

    Refactor REST tests

--- tests/rest.cpp
+++ tests/rest.cpp
@@ -29,13 +29,13 @@ std::string environment_variable(std::string name)
     }
 }
 
-TEST_CASE( "Correct entity ids are returned", "[!throws]" ) {
+SCENARIO("REST api is successful", "[!mayfail]") {
     std::string token = environment_variable("DISCCORD_TEST_TOKEN");
     std::string user_id = environment_variable("DISCCORD_TEST_USER_ID");
 
     if (token.empty())
     {
-        throw std::runtime_error("Pass a **BOT** token through the environment variable `DISCCORD_TEST_TOKEN`");
+        FAIL("Pass a **BOT** token through the environment variable `DISCCORD_TEST_TOKEN`");
     }
 
     if (user_id.empty())
@@ -46,40 +46,54 @@ TEST_CASE( "Correct entity ids are returned", "[!throws]" ) {
 
     uint64_t id = std::stoull(user_id);
 
-    auto api_client = rest_api_client(web::uri("https://discordapp.com/api/v6"), token, disccord::token_type::Bot);
+    GIVEN("an api client with our token and id") {
+        auto api_client = rest_api_client(web::uri("https://discordapp.com/api/v6"), token, disccord::token_type::Bot);
 
-    api_client.get_current_user().then([=](user ent)
-    {
-        CAPTURE(ent.get_id());
-        REQUIRE(ent.get_id() == id);
-    }).wait();
+        WHEN("we retrieve the current user") {
+            uint64_t retrieved_id;
 
-    std::stringstream message_builder;
+            api_client.get_current_user().then([&](user ent)
+            {
+                CAPTURE(ent.get_id());
+                retrieved_id = ent.get_id();
+            }).wait();
 
-    message_builder
-    << "Hello, world! Message sent using disccord "
-    << DISCCORD_VERSION
-    << " located at <"
-    << DISCCORD_PROJECT_URL
-    << ">!" << "\n";
+            THEN("the id equals what we are given") {
+                REQUIRE(retrieved_id == id);
+            }
+        }
 
-    std::string ci_id = environment_variable("TRAVIS_BUILD_NUMBER");
-    std::string ci_commit = environment_variable("TRAVIS_COMMIT");
+        WHEN("we send a message") {
+            std::stringstream message_builder;
 
-    if (ci_id != "")
-    {
-        message_builder
-        << "**Build ID:** " << ci_id << "\n"
-        << "**Commit:** " << ci_commit << "\n";
+            message_builder
+            << "Hello, world! Message sent using disccord "
+            << DISCCORD_VERSION
+            << " located at <"
+            << DISCCORD_PROJECT_URL
+            << ">!" << "\n";
+
+            std::string ci_id = environment_variable("TRAVIS_BUILD_NUMBER");
+            std::string ci_commit = environment_variable("TRAVIS_COMMIT");
+
+            if (ci_id != "")
+            {
+                message_builder
+                << "**Build ID:** " << ci_id << "\n"
+                << "**Commit:** " << ci_commit << "\n";
+            }
+
+            bool success;
+
+            api_client.create_message(237990708101775361, message_builder.str()).then([&](message msg)
+            {
+                success = true;
+            }).wait();
+
+            THEN("the send succeeds") {
+                if (!success)
+                    FAIL("create_message failed!");
+            }
+        }
     }
-
-    bool success;
-
-    api_client.create_message(237990708101775361, message_builder.str()).then([&](message msg)
-    {
-        success = true;
-    }).wait();
-
-    if (!success)
-        FAIL("create_message failed!");
 }
