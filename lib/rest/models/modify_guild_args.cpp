3d304f1efb90ff70daee32d3e4754168aee38a4a
Ashton Tito <ashton_tito@sfu.ca>
2017-04-07 16:50:26 -0700

    build more rest models

--- lib/rest/models/modify_guild_args.cpp
+++ lib/rest/models/modify_guild_args.cpp
@@ -6,8 +6,7 @@ namespace disccord
     {
         namespace models
         {
-            modify_guild_args::modify_guild_args(std::string _name)
-            : name(_name)
+            modify_guild_args::modify_guild_args()
             { }
             
             modify_guild_args::~modify_guild_args()
@@ -15,13 +14,39 @@ namespace disccord
 
             void modify_guild_args::encode_to(std::unordered_map<std::string, web::json::value>& info)
             {
-                info["name"] = web::json::value(get_name());
+                #define encode_field(var) \
+                    if (var.is_specified()) \
+                        info[#var] = web::json::value(var.get_value());
+                        
+                encode_field(name);
+                encode_field(region);
+                encode_field(icon);
+                encode_field(splash);
+                encode_field(afk_channel_id);
+                encode_field(owner_id);
+                encode_field(verification_level);
+                encode_field(default_message_notifications);
+                encode_field(afk_timeout);
+                
+                #undef encode_field
             }
 
-            std::string modify_guild_args::get_name()
-            {
-                return name;
-            }
+            #define define_set_method(field_name, type) \
+                void modify_guild_args::set_##field_name(type val) { \
+                    field_name = decltype(field_name)(val); \
+                }
+            
+            define_set_method(name, std::string)
+            define_set_method(region, std::string)
+            define_set_method(icon, std::string)
+            define_set_method(splash, std::string)
+            define_set_method(afk_channel_id, uint64_t)
+            define_set_method(owner_id, uint64_t)
+            define_set_method(verification_level, uint32_t)
+            define_set_method(default_message_notifications, uint32_t)
+            define_set_method(afk_timeout, uint32_t)
+            
+            #undef define_set_method
         }
     }
 }
