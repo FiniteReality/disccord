47400f224e26e9abc971d37cb3a55f7c99a4e1a8
Ashton Tito <ashton_tito@sfu.ca>
2017-04-11 16:40:28 -0700

    format

--- lib/rest/client.cpp
+++ lib/rest/client.cpp
@@ -11,6 +11,7 @@ namespace disccord
         discord_rest_client::discord_rest_client(std::string token)
         : api_client(web::uri(DISCORD_BASE_URL), token, disccord::token_type::Bot)
         { }
+        
         discord_rest_client::discord_rest_client(std::string token, disccord::token_type type)
         : api_client(web::uri(DISCORD_BASE_URL), token, type)
         { }
