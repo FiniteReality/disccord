00cb2cf9cd1626f788d1ef8a7764dfaa50e2bd49
Ashton Tito <ashton_tito@sfu.ca>
2017-04-11 14:07:32 -0700

    config ctor

--- lib/rest/client.cpp
+++ lib/rest/client.cpp
@@ -5,10 +5,22 @@ namespace disccord
     namespace rest
     {
         discord_rest_client::discord_rest_client()
-            : api_client(web::uri(DISCORD_BASE_URL), "", disccord::token_type::Bot)
+        : api_client(web::uri(DISCORD_BASE_URL), "", disccord::token_type::Bot)
+        { }
+
+        discord_rest_client::discord_rest_client(std::string token)
+        : api_client(web::uri(DISCORD_BASE_URL), token, disccord::token_type::Bot)
+        { }
+        discord_rest_client::discord_rest_client(std::string token, disccord::token_type type)
+        : api_client(web::uri(DISCORD_BASE_URL), token, type)
         { }
 
         discord_rest_client::~discord_rest_client()
         { }
+        
+        disccord::rest::internal::rest_api_client discord_rest_client::get_client()
+        {
+            return api_client;
+        }
     }
 }
