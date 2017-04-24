386e09c60b65d916ce00912c4cbe72a52e5684e3
Ashton Tito <ashton_tito@sfu.ca>
2017-04-24 05:24:20 -0700

    accept file stream for setting avatar isntead of raw base64 string
    we will do the proper converting of the image data internally, user just needs to specify their file stream

--- include/disccord/rest/models/modify_current_user_args.hpp
+++ include/disccord/rest/models/modify_current_user_args.hpp
@@ -1,8 +1,13 @@
 #ifndef _modify_current_user_args_hpp_
 #define _modify_current_user_args_hpp_
 
+#include <cpprest/http_client.h>
+
 #include <disccord/models/model.hpp>
 #include <disccord/util/optional.hpp>
+
+using namespace Concurrency::streams;
+using namespace utility::conversions;
 
 namespace disccord
 {
@@ -17,13 +22,15 @@ namespace disccord
                     virtual ~modify_current_user_args();
 
                     void set_name(std::string name);
-                    void set_avatar(std::string avatar);
+                    //avatar_stream -> file_stream<unsigned char>::open_istream(<filename>).get()
+                    //NOTE: using an unsigned char stream because the to_base64 method takes a std::vector<unsigned char>
+                    void set_avatar(basic_istream<unsigned char> avatar_stream);
 
                 protected:
                     virtual void encode_to(std::unordered_map<std::string, web::json::value>& info) override;
 
                 private:
-                    util::optional<std::string> name, avatar; //avatar must be passed image data converted to base64 data
+                    util::optional<std::string> name, avatar;
             };
         }
     }
