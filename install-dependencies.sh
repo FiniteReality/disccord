7852712e228bb06f57610cac42e1d292ec4aceef
FiniteReality <digpoealternate@gmail.com>
2017-07-10 20:09:01 +0100

    Fix install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env sh
 
 git clone https://github.com/microsoft/cpprestsdk casablanca
-pushd casablanca/Release
+cd casablanca/Release
 mkdir build.release
-pushd build.release
+cd build.release
 cmake .. -DCMAKE_BUILD_TYPE=Release
-make
-sudo make install
-
-popd
-popd
-
-git clone https://github.com/ruediger/libbert libbert
-pushd libbert
-./configure
 make
 sudo make install
