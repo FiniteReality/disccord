b93dc9e955399a7f3fabd32dda2521257f70dd3b
FiniteReality <digpoealternate@gmail.com>
2017-07-23 11:56:32 +0100

    More work on models

--- models/user.lua
+++ models/user.lua
@@ -2,7 +2,7 @@ model{"user",
     property{"username", "std::string"},
     property{"discriminator", "disccord::discriminator"},
     property{"avatar", "disccord::util::optional<std::string>"},
-    property{"bot", "bool"},
+    property{"bot", "disccord::util::optional<bool>"},
     property{"mfa_enabled", "disccord::util::optional<bool>"},
     property{"verified", "disccord::util::optional<bool>"},
     property{"email", "disccord::util::optional<std::string>"}
